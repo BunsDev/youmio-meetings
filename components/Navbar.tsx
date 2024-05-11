0ca7075c17b6012dbf07426633ac8fe7218c5c0f
sensurusirupus <sensurusirupus@gmail.com>
2024-05-11 08:21:55 +0000

    update 5 files

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,7 @@ import MobileNav from './MobileNav';
 
 const Navbar = () => {
   return (
-    <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+    <nav className="flex-between fixed z-50 w-full bg-[#0f0f0f] shadow-xl px-6 py-4 lg:px-10">
       <Link href="/" className="flex items-center gap-1">
         <Image
           src="/icons/logo.svg"
@@ -15,11 +15,11 @@ const Navbar = () => {
           alt="yoom logo"
           className="max-sm:size-10"
         />
-        <p className="text-[26px] font-extrabold text-white max-sm:hidden">
-          YOOM
+        <p className="text-[26px] font-medium text-white max-sm:hidden">
+          Youmio
         </p>
       </Link>
-      <div className="flex-between gap-5">
+      <div className="flex-between gap-5 w-10 h-10">
         <SignedIn>
           <UserButton afterSignOutUrl="/sign-in" />
         </SignedIn>
