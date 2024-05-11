811238216025a2d201dc1342ace7416889cf0a8f
sensurusirupus <sensurusirupus@gmail.com>
2024-05-11 10:43:03 +0000

    update 6 files

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,6 +1,9 @@
+'use client';
 import MeetingTypeList from '@/components/MeetingTypeList';
 import Image from 'next/image';
-
+import React from 'react';
+import { WobbleCard } from '@/components/ui/wobble-card';
+import { IconBroadcast } from '@tabler/icons-react';
 const Home = () => {
   const now = new Date();
 
@@ -14,6 +17,58 @@ const Home = () => {
 
   return (
     <section className="flex size-full flex-col gap-4 text-white">
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 max-w-7xl mx-auto w-full">
+        <WobbleCard
+          containerClassName="col-span-1 lg:col-span-2 h-full bg-pink-800 min-h-[500px] lg:min-h-[300px]"
+          className=""
+        >
+          <div className="max-w-xs">
+            <h2 className="text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
+              Watch Livestreams from your Friends
+              <IconBroadcast className="h-20 w-20" />
+            </h2>
+            <p className="mt-4 text-left  text-base/6 text-neutral-200">
+              With over 1,000 mothly active bot users, Youmio is the most
+              popular streaming and meeting platform for streamers.
+            </p>
+          </div>
+          <Image
+            src="/images/music-lady.png"
+            width={600}
+            height={600}
+            alt="linear demo image"
+            className="absolute -right-4 lg:-right-[5%] grayscale filter -bottom-10 object-contain rounded-2xl"
+          />
+        </WobbleCard>
+        <WobbleCard containerClassName="col-span-1 min-h-[300px]">
+          <h2 className="max-w-80  text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
+            No shirt, no shoes, no weapons.
+          </h2>
+          <p className="mt-4 max-w-[26rem] text-left  text-base/6 text-neutral-200">
+            If someone yells “stop!”, goes limp, or taps out, the fight is over.
+          </p>
+        </WobbleCard>
+        <WobbleCard containerClassName="col-span-1 lg:col-span-3 bg-blue-900 min-h-[500px] lg:min-h-[600px] xl:min-h-[300px]">
+          <div className="max-w-sm">
+            <h2 className="max-w-sm md:max-w-lg  text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
+              Signup for blazing-fast cutting-edge state of the art Gippity AI
+              wrapper today!
+            </h2>
+            <p className="mt-4 max-w-[26rem] text-left  text-base/6 text-neutral-200">
+              With over 100,000 mothly active bot users, Gippity AI is the most
+              popular AI platform for developers.
+            </p>
+          </div>
+          <Image
+            src="/linear.webp"
+            width={500}
+            height={500}
+            alt="linear demo image"
+            className="absolute -right-10 md:-right-[40%] lg:-right-[20%] -bottom-10 object-contain rounded-2xl"
+          />
+        </WobbleCard>
+      </div>
+
       <div className="h-[303px] w-full rounded-[20px] bg-hero bg-cover">
         <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
           <h2 className="glassmorphism max-w-[273px] rounded-xl py-2 text-center text-base font-normal">
