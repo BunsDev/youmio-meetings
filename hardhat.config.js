4d9ac16d878934f7e5870e10b76a52662dd133ce
sensurusirupus <sensurusirupus@gmail.com>
2024-05-16 11:30:07 +0000

    update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -30,12 +30,6 @@ module.exports = {
       url: 'https://polygon-mumbai.g.alchemy.com/v2/2bGIFu-iEnl9RvAOTe1ddZI2gBnuYQGS',
       accounts: [privateKey],
     },
-    kovan: {
-      url: 'https://kovan.infura.io/v3/745fcbe1f649402c9063fa946fdbb84c',
-      accounts: [privateKey],
-      gas: 2100000,
-      gasPrice: 8000000000,
-    },
   },
   solidity: '0.8.24',
   allowUnlimitedContractSize: true,
